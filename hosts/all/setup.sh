64467d8a9f1ed23f376176e90de30563bc942823
juodumas <juodumas@gmail.com>
2023-04-30 02:29:25 +0300

    fix webcam script path

--- hosts/all/setup.sh
+++ hosts/all/setup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -e
 
 sed -i '/stream-webcam.sh/d; /^exit 0/d' /etc/rc.local
-echo "/root/setup/scripts/stream-webcam.sh" >> /etc/rc.local
-if [ -d "$script_dir/user-home-dir" ]; then
-    rsync -a --chown user:user "$script_dir/user-home-dir/" /home/user/
+echo "/root/setup/hosts/all/scripts/stream-webcam.sh" >> /etc/rc.local
+if [ -d "user-home-dir" ]; then
+    rsync -a --chown user:user "user-home-dir/" /home/user/
 fi
