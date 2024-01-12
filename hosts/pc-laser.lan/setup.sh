8a121913c4cc0fc6960aca7b53084d3e364bc22d
juodumas <juodumas@gmail.com>
2024-01-12 21:42:33 +0200

    laser pc: use direct TCP X11 connection instead of SSH
    (beaglebone was struggling with X11 over SSH)

--- hosts/pc-laser.lan/setup.sh
+++ hosts/pc-laser.lan/setup.sh
@@ -1,5 +1,9 @@
 #!/bin/sh -e
 
+# Allow tcp connection to the X server.
+# Access is restricted by xhost (e.g. /etc/X11/Xsession.d/35x11-common_xhost-local)
+sed -i 's/^#xserver-allow-tcp=false.*/xserver-allow-tcp=true/' /etc/lightdm/lightdm.conf
+
 if [ -d "user-home-dir" ]; then
     rsync -a --chown user:user "user-home-dir/" /home/user/
     chmod 600 /home/user/.ssh/id_rsa
