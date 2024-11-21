6666bef7cfb2e524a2b09b5c7c3fb1af769fa2f7
juodumas <juodumas@gmail.com>
2024-11-21 03:13:40 +0200

    chromium: setup homepage

--- hosts/all/setup.sh
+++ hosts/all/setup.sh
@@ -5,3 +5,7 @@ echo "/root/setup/hosts/all/scripts/stream-webcam.sh" >> /etc/rc.local
 if [ -d "user-home-dir" ]; then
     rsync -a --chown user:user "user-home-dir/" /home/user/
 fi
+if [ -d /etc/chromium ]; then
+    cp -r chromium-settings/* /etc/chromium/
+    echo 'export CHROMIUM_FLAGS="$CHROMIUM_FLAGS --homepage file:///etc/chromium/homepage.html"' > /etc/chromium.d/homepage
+fi
